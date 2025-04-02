39461e72ac1e6d2a0c95bcf5a30cf28ad5d0ebff
Meryem Uludağlı <meryemuslu2035@gmail.com>
2025-04-02 16:51:47 +0300

    add

--- backend/methods/post.js
+++ backend/methods/post.js
@@ -1,8 +1,30 @@
 const bodyParser = require("../utils/bodyParser");
+const crypto = require("crypto");
 
 const postRequest = async (req, res) => {
   if (req.url === "/api/movies") {
     const body = await bodyParser(req);
+
+    const keys = [
+      "title",
+      "year",
+      "rating",
+      "description",
+      "language",
+      "director",
+    ];
+
+    if (
+      keys.some((key) => !body[key]) ||
+      !body.genre.length > 0 ||
+      !body.cast.length > 0
+    ) {
+      res.writeHead(404);
+      res.end("Lutfen zounlu alan butun alanlari tanimlayin");
+      return;
+    }
+
+    body.id = crypto.randomUUID();
     res.end("film olusturuldu");
   } else {
     res.writeHead(404);
